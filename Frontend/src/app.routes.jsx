f806b513910ef5e3a9edbe37c6fb8b487965194d
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-07-07 10:20:04 +0530

    api

--- Frontend/src/app.routes.jsx
+++ Frontend/src/app.routes.jsx
@@ -3,7 +3,7 @@ import Login from "./features/auth/pages/Login.jsx"
 import Register from "./features/auth/pages/Register.jsx"
 import Protected from "./features/auth/components/Protected";
 import Home from "./features/interview/pages/Home.jsx";
-import Interview from "./features/interview/pages/interview.jsx";
+import Interview from "./features/interview/pages/Interview.jsx";
 
 export const router = createBrowserRouter([
     {
