cb22c7718c705f9b7590090943d45778496f6a4e
Nitinsingh200 <nitinsinghna12345@gamil.com>
2026-07-08 19:18:04 +0530

    new change

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,12 @@ app.use(cors({
 const authRouter = require("./routes/auth.routes")
 const interviewRouter = require("./routes/interview.routes")
 
-
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "SkillBridge AI Backend is running 🚀"
+    });
+});
 /* using all the routes here */
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
